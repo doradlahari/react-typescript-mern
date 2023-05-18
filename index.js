1f1a56daefd595d62f3a62b387c2f35a158adb98
Doradla Hari <doradla.hari@gmail.com>
2023-05-18 18:13:49 +0530

    implemented signup login email sending validations

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const createUserDetails = require('./signup')
 const app = express();
 const middleware = require('./middleware')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
 app.use(express.json()); // mididle ware
-
+app.use(cors({ origin: '*' }));
+const nodemailer = require('nodemailer');
 mongoose
     .connect("mongodb+srv://haridevworld2022:merntypescript@cluster0.zx2nmhm.mongodb.net/userdata", {
         useNewUrlParser: true,
@@ -18,6 +20,41 @@ mongoose
     .catch((err) => {
         console.error("DB connection error:", err);
     });
+
+
+
+// Nodemailer setup
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'doradla.hari@gmail.com',
+        pass: 'erkluzuuibckjyan'
+    }
+});
+
+// Function to send welcome email
+const sendWelcomeEmail = (email) => {
+    const mailOptions = {
+        from: 'haridevworld2022@gmail.com',
+        to: email,
+        subject: 'Welcome to Our Application',
+        text: 'Thank you for registering with our application. We are excited to have you on board!',
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending welcome email:', error);
+        } else {
+            console.log('Welcome email sent:', info.response);
+            res.status(200).send(info.response)
+        }
+    });
+};
+
+
+
 app.get("/", (req, res) => {
     res.send("Hello hari");
 });
@@ -38,16 +75,18 @@ app.post("/loginuser", async (req, res) => {
         const loggedInUserData = new loginUserDetails({ email, password });
         let exist = await createUserDetails.findOne({ email: email })
         if (!exist) {
-            return res.status(400).send("user not found")
+            return res.send("user not found")
         }
         if (exist.password !== password) {
-            return res.status(400).send("invalid credentails")
+            return res.send("invalid credentails")
         }
         let payload = {
             user: {
                 id: exist.id
             }
         }
+        // Set the token in the browser headers
+
         jwt.sign(payload, "jwtSecret", { expiresIn: 3600000 },
             (err, token) => {
                 if (err) throw err;
@@ -55,6 +94,7 @@ app.post("/loginuser", async (req, res) => {
             }
         )
         await loggedInUserData.save();
+        res.setHeader('Authorization', `Bearer ${token}`);
         res.status(200).send("successfully loggedin!")
     } catch (err) {
         console.log(err);
@@ -67,7 +107,7 @@ app.get("/myprofile", middleware, async (req, res) => {
     try {
         let exist = await createUserDetails.findById(req.user.id)
         if (!exist) {
-            return res.status(400).send('user not found')
+            return res.send('user not found')
         }
         res.json(exist)
     }
@@ -88,17 +128,20 @@ app.post("/createuser", async (req, res) => {
         const newUserData = new createUserDetails({ firstName, lastName, email, mobileNumber, password, conformPassword });
         let exist = await createUserDetails.findOne({ email: email })
         if (exist) {
-            return res.status(400).send('user alredy exists')
+            return res.send('user alredy exists')
         }
         if (password !== conformPassword) {
-            return res.status(400).send('password mismatch')
+            return res.send('password mismatch')
         }
         await newUserData.save();
-        res.status(200).send("registration sucessfull!")
+        // res.status(200).send("registration sucessfull!")
+        // Send welcome email
+        sendWelcomeEmail(email);
+        res.status(200).send(`Hello, ${firstName} your registration sucessfull! and you have received an email to this email ${email}`)
     } catch (err) {
         console.log(err);
         return res.status(500).send("internal server error!")
     }
 });
 
-app.listen(3000, () => console.log("server running............"));
+app.listen(5000, () => console.log("server running............"));
