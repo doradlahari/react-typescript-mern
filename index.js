cc14f73c6128a931b51d6a3a7243f5bbf8242e96
Doradla Hari <doradla.hari@gmail.com>
2023-05-25 11:55:27 +0530

    added files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const middleware = require('./middleware')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
+const fs = require('fs')
 app.use(express.json()); // mididle ware
 app.use(cors({ origin: '*' }));
 const nodemailer = require('nodemailer');
@@ -23,7 +24,48 @@ mongoose
         console.error("DB connection error:", err);
     });
 
+// video streaming
+const videoFileMap = {
+    "cdn": 'videos/cdn.mp4',
+    'generate-pass': 'videos/generate-pass.mp4',
+    'get-post': 'videos/get-post.mp4',
+    'index-video': 'videos/index-video.mp4',
+    'cricket': 'videos/cricket.mp4'
+}
+app.get('/videos/:filename', (req, res) => {
+    const fileName = req.params.filename
+    const filePath = videoFileMap[fileName]
+    if (!filePath) {
+        return res.status(404).send('File not found')
+    }
+    const stat = fs.statSync(filePath)
+    const fileSize = stat.size
+    const range = req.headers.range
 
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+
+        const chunkSize = end - start + 1
+        const file = fs.createReadStream(filePath, { start, end })
+        const head = {
+            "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": chunkSize,
+            "Content-Type": "video/mp4",
+        };
+        return res.writeHead(206, head),
+            file.pipe(res)
+    } else {
+        const head = {
+            "Content-Length": fileSize,
+            "Content-Type": "video/mp4",
+        };
+        return res.writeHead(200, head),
+            fs.createReadStream(filePath).pipe(res)
+    }
+})
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
@@ -146,5 +188,5 @@ app.post("/createuser", async (req, res) => {
     }
 });
 
-app.listen(5000, () => console.log("server running............"));
+app.listen(5000, () => console.log("server running -> auth + video streaming............"));
 module.exports = app
